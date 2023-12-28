4f8a251d64fd2e20629141a415b82ba6d10e8ab8
FaleteRusso <faleterusso89@gmail.com>
2023-12-28 19:00:18 +0100

    yes

--- src/com/corenetworks/Presentacion/ProbarRopaLeer.java
+++ src/com/corenetworks/Presentacion/ProbarRopaLeer.java
@@ -1,2 +1,46 @@
-package com.corenetworks.Presentacion;public class ProbarRopaLeer {
-}
+package com.corenetworks.Presentacion;
+
+import com.corenetworks.modelo.Camisa;
+import com.corenetworks.modelo.Pantalon;
+import com.corenetworks.modelo.Zapatos;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ProbarRopaLeer {
+    public static void main(String[] args) {
+        FileInputStream fEntrada;
+        ObjectInputStream objeto;
+       Pantalon p1 =null;
+        Pantalon p2 = null;
+        Camisa c1= null;
+        Zapatos z1= null;
+        Zapatos z2 = null;
+        //Acciones de E/S
+        try {
+            fEntrada = new FileInputStream("Ropa.dat");
+            objeto = new ObjectInputStream(fEntrada);
+            p1=(Pantalon) objeto.readObject();
+            System.out.println(p1.toString());
+            p2=(Pantalon) objeto.readObject();
+            System.out.println(p2.toString());
+            c1=(Camisa) objeto.readObject();
+            System.out.println(c1.toString());
+            z1=(Zapatos) objeto.readObject();
+            System.out.println(z1.toString());
+            z2=(Zapatos) objeto.readObject();
+            System.out.println(z2.toString());
+            //cierre streams
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+
+    }
+    }
+
--- src/com/corenetworks/Presentacion/ProbarRopaOutput.java
+++ src/com/corenetworks/Presentacion/ProbarRopaOutput.java
@@ -1,2 +1,47 @@
-package com.corenetworks.Presentacion;public class ProbarRopaOutput {
+package com.corenetworks.Presentacion;
+
+import com.corenetworks.modelo.Camisa;
+import com.corenetworks.modelo.Pantalon;
+import com.corenetworks.modelo.Zapatos;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class ProbarRopaOutput {
+    public static void main(String[] args) {
+        //declara variables
+        FileOutputStream fSalida;
+        ObjectOutputStream objeto;
+        Pantalon p1;
+        Pantalon p2;
+        Camisa c1;
+        Zapatos z1;
+        Zapatos z2;
+        //acciones E/S
+        try {
+            fSalida = new FileOutputStream("Ropa.dat");
+            objeto = new ObjectOutputStream(fSalida);
+            p1=new Pantalon("Largo");
+            objeto.writeObject(p1);
+            p2=new Pantalon("Vaquero largo");
+            objeto.writeObject(p2);
+            c1= new Camisa(false,36);
+            objeto.writeObject(c1);
+            z1= new Zapatos(9);
+            objeto.writeObject(z1);
+            z2=new Zapatos(9);
+            objeto.writeObject(z2);
+            //cerrar
+            objeto.flush();
+            objeto.close();
+            fSalida.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+
 }
